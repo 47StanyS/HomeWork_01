40e1db356b5ee03319e510ae5cd656af3e64caae
47StanyS <stanyslavalokhin@gmail.com>
2024-04-11 22:12:33 +0300

    Additional task,

--- Assets/Scripts/LocationBuilder.cs
+++ Assets/Scripts/LocationBuilder.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class LocationBuilder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject prefab1;
+    [SerializeField] private GameObject prefab2;
+    [SerializeField] private GameObject prefab3;
     void Start()
     {
-        
+        // GameObject player = new GameObject ("Player", typeof(Rigidbody), typeof(BoxCollider));
+
+        // player.transform.position = new Vector3(0,5,0);
+
+        // GameObject ground = new GameObject ("Ground", typeof (Rigidbody), typeof(BoxCollider));
+        // ground.transform.position = new Vector3(0,0,0);
+
+        // GameObject enemy = new GameObject ("Enemy", typeof(Rigidbody),typeof(BoxCollider));
+        // enemy.transform.position = new Vector3(0,15,0);
+
+
+        // transform.position = new Vector3(10, 15, 0);
+
+        for (var i = 0; i < 10; i++)
+        {
+            float randomX = Random.Range(-4.0f,4.0f);
+            int randomY = Random.Range(10, 60);
+
+            Vector3 spawnPosition = new Vector3(randomX, randomY, 0);
+
+            Instantiate(prefab1, spawnPosition, Quaternion.identity);
+            Instantiate(prefab2, spawnPosition, Quaternion.identity);
+            Instantiate(prefab3, spawnPosition, Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
